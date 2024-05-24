a66edf35a43767572dad34ee872fb4e97380491a
ndridm2 <andri.darmuli26@gmail.com>
2024-05-24 19:54:54 +0700

    feat(api): add api wisata backend

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// login
+Route::post('/login', [AuthController::class, 'login']);
+// logout
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// product
+Route::apiResource('/api-products', ProductController::class)->middleware('auth:sanctum');
+// categories
+Route::apiResource('/api-categories', CategoryController::class)->middleware('auth:sanctum');
